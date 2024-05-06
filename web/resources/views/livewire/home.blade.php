4f737280b9a5fe60edd41d2daac323641e81f12e
Albin Varghese <albin8285@gmail.com>
2024-05-07 00:32:11 +0530

    feat: update project

---
+++ web/resources/views/livewire/home.blade.php
@@ -0,0 +1,94 @@
+<!-- Grid -->
+<div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6" wire:poll.500ms>
+    <!-- Card -->
+    <div class="flex flex-col gap-y-3 lg:gap-y-5 p-4 md:p-5 bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-800">
+        <div class="inline-flex justify-center items-center">
+            <span class="size-2 inline-block bg-gray-500 rounded-full me-2"></span>
+            <span class="text-sm font-semibold uppercase text-gray-600">APPLIANCE 1</span>
+        </div>
+
+        <div class="text-center">
+            <h3 class="text-3xl sm:text-4xl lg:text-5xl font-semibold text-green-800">
+                ON
+            </h3>
+        </div>
+
+        <dl class="flex justify-center items-center divide-x divide-gray-200 dark:divide-neutral-800">
+            <dt class="pe-3">
+          <span class="text-green-600">
+            <span class="inline-block text-sm">
+                 ESP32 Server
+            </span>
+          </span>
+                <span class="block text-sm text-gray-500 dark:text-neutral-500">change</span>
+            </dt>
+            <dd class="text-start ps-3">
+                <span class="text-sm font-semibold text-gray-800 dark:text-neutral-200">5</span>
+                <span class="block text-sm text-gray-500 dark:text-neutral-500">last week</span>
+            </dd>
+        </dl>
+    </div>
+    <!-- End Card -->
+
+    <!-- Card -->
+    <div class="flex flex-col gap-y-3 lg:gap-y-5 p-4 md:p-5 bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-800">
+        <div class="inline-flex justify-center items-center">
+            <span class="size-2 inline-block bg-green-500 rounded-full me-2"></span>
+            <span class="text-sm font-semibold uppercase text-gray-600">APPLIANCE 2</span>
+        </div>
+
+        <div class="text-center">
+            <h3 class="text-3xl sm:text-4xl lg:text-5xl font-semibold text-red-800">
+                OFF
+            </h3>
+        </div>
+
+        <dl class="flex justify-center items-center divide-x divide-gray-200 dark:divide-neutral-800">
+            <dt class="pe-3">
+          <span class="text-green-600">
+
+            <span class="inline-block text-sm">
+              ESP32 Server
+            </span>
+          </span>
+                <span class="block text-sm text-gray-500 dark:text-neutral-500">change</span>
+            </dt>
+            <dd class="text-start ps-3">
+                <span class="text-sm font-semibold text-gray-800 dark:text-neutral-200">7</span>
+                <span class="block text-sm text-gray-500 dark:text-neutral-500">last week</span>
+            </dd>
+        </dl>
+    </div>
+    <!-- End Card -->
+
+    <!-- Card -->
+    <div class="flex flex-col gap-y-3 lg:gap-y-5 p-4 md:p-5 bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-800">
+        <div class="inline-flex justify-center items-center">
+            <span class="size-2 inline-block bg-red-500 rounded-full me-2"></span>
+            <span class="text-xs font-semibold uppercase text-gray-600 dark:text-neutral-400">Failed conversions</span>
+        </div>
+
+        <div class="text-center">
+            <h3 class="text-3xl sm:text-4xl lg:text-5xl font-semibold text-gray-800 dark:text-neutral-200">
+                4
+            </h3>
+        </div>
+
+        <dl class="flex justify-center items-center divide-x divide-gray-200 dark:divide-neutral-800">
+            <dt class="pe-3">
+          <span class="text-red-600">
+            <span class="inline-block text-sm">
+              ESP32 Server
+            </span>
+          </span>
+                <span class="block text-sm text-gray-500 dark:text-neutral-500">change</span>
+            </dt>
+            <dd class="text-start ps-3">
+                <span class="text-sm font-semibold text-gray-800 dark:text-neutral-200">7</span>
+                <span class="block text-sm text-gray-500 dark:text-neutral-500">last week</span>
+            </dd>
+        </dl>
+    </div>
+    <!-- End Card -->
+</div>
+<!-- End Grid -->
